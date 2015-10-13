75deb2d8d9b255f74c6a9751e9885b9bbf23278d
Preetam D'Souza <preetamjdsouza@gmail.com>
2015-10-13 16:36:20 -0700

    Maru UI: add dynamic mirroring QuickSettings tile
    (cherry picked from commit 194c7a81d12027b0ddab556a5784c4b76e4d4fc2 with
    changes for O)
    
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.MMirrorTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
@@ -89,6 +90,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("sync")) return new SyncTile(mHost);
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(mHost);
         else if (tileSpec.equals("volume_panel")) return new VolumeTile(mHost);
+        else if (tileSpec.equals("mmirror")) return new MMirrorTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
---
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/MMirrorTile.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2015 Preetam D'Souza
+ *
+ * QuickSettings HDMI mirroring toggle.
+ *
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.display.DisplayManager;
+import android.util.Log;
+import android.view.Display;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.R;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+
+/** Quick settings tile: Mirror screen **/
+public class MMirrorTile extends QSTileImpl<BooleanState> {
+    private static final String TAG = "MMirrorTile";
+
+    private static final int mDisabledIcon = R.drawable.ic_mirroring_disabled;
+    private static final int mEnabledIcon = R.drawable.ic_mirroring_enabled;
+
+    private final DisplayManager mDisplayManager;
+
+    private final MDisplayListener mDisplayListener;
+    // track the hdmi display id to check if it has been removed later
+    private int mHdmiDisplayId = -1;
+    private boolean mListening = false;
+
+    public MMirrorTile(QSHost host) {
+        super(host);
+
+        mDisplayManager = (DisplayManager) host.getContext()
+                .getSystemService(Context.DISPLAY_SERVICE);
+
+        mDisplayListener = new MDisplayListener();
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleClick() {
+        if (mState.value) {
+            Log.d(TAG, "disabling mirroring!");
+            mDisplayManager.disableMirroring();
+        } else {
+            Log.d(TAG, "enabling mirroring!");
+            mDisplayManager.enableMirroring();
+        }
+        refreshState();
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        final boolean hasHdmiDisplay = mHdmiDisplayId != -1;
+        state.value = mDisplayManager.isMirroringEnabled();
+        state.label = mContext.getString(R.string.quick_settings_mirroring_mode_label);
+        state.icon = ResourceIcon.get(state.value ? mEnabledIcon : mDisabledIcon);
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(R.string.accessibility_qs_mirroring_changed_on);
+        } else {
+            return mContext.getString(R.string.accessibility_qs_mirroring_changed_off);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.QS_MMIRROR_TOGGLE;
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_mirroring_mode_label);
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        // defense against duplicate registers
+        if (mListening == listening) {
+            return;
+        }
+
+        if (listening) {
+            Log.d(TAG, "registering mDisplayListener");
+            mDisplayListener.sync();
+            mDisplayManager.registerDisplayListener(mDisplayListener, null);
+        } else {
+            Log.d(TAG, "unregistering mDisplayListener");
+            mDisplayManager.unregisterDisplayListener(mDisplayListener);
+        }
+        mListening = listening;
+    }
+
+    private class MDisplayListener implements DisplayManager.DisplayListener {
+        @Override
+        public void onDisplayAdded(int displayId) {
+            Display display = mDisplayManager.getDisplay(displayId);
+            Log.d(TAG, "Display added: " + display);
+            final boolean hdmiDisplayAdded = display.getType() == Display.TYPE_HDMI;
+
+            if (hdmiDisplayAdded) {
+                if (mHdmiDisplayId == -1) {
+                    mHdmiDisplayId = displayId;
+                    refreshState();
+                }
+            }
+        }
+
+        @Override
+        public void onDisplayRemoved(int displayId) {
+            if (displayId == mHdmiDisplayId) {
+                if (mHdmiDisplayId != -1) {
+                    mHdmiDisplayId = -1;
+                    refreshState();
+                }
+            }
+        }
+
+        @Override
+        public void onDisplayChanged(int displayId) { /* no-op */ }
+
+        /**
+         * We may miss a display event since listeners are unregistered
+         * when the QS panel is hidden.
+         *
+         * Call this before registering to make sure the initial
+         * state is up-to-date.
+         */
+        public void sync() {
+            mHdmiDisplayId = -1;
+            Display[] displays = mDisplayManager
+                    .getDisplays(DisplayManager.DISPLAY_CATEGORY_PRESENTATION);
+            for (Display display : displays) {
+                if (display.getType() == Display.TYPE_HDMI) {
+                    mHdmiDisplayId = display.getDisplayId();
+                    break;
+                }
+            }
+        }
+    }
+}
