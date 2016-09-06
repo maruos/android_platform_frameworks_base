7102f123afd4ea5f4d3dcbcfab81d11ea2a4b24c
Preetam D'Souza <preetamjdsouza@gmail.com>
2016-09-06 14:41:13 -0400

    Update copyright notices after M merge
    * Add missing copyright notice
    * Remove unnecessary copyright notice
    
    There was a refactor in M that moved the service registry out of
    ContextImpl, so there's nothing from The Maru OS Project here anymore.
    It's identical to AOSP's version now.
    
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -1,7 +1,5 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
- * Copyright (C) 2015-2016 Preetam J. D'Souza
- * Copyright (C) 2016 The Maru OS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
